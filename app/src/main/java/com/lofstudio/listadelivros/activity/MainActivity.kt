b0101b23d4ed4cd11780c06191f1e2355581a212
LOFStudio <dev.lofstudio@gmail.com>
2023-01-16 16:21:43 -0300

    230116

--- app/src/main/java/com/lofstudio/listadelivros/activity/MainActivity.kt
+++ app/src/main/java/com/lofstudio/listadelivros/activity/MainActivity.kt
@@ -1,15 +1,20 @@
-package com.lofstudio.listadelivros
+package com.lofstudio.listadelivros.activity
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lofstudio.listadelivros.activity.BuscarLivrosActivity
+import com.lofstudio.listadelivros.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //COSTUMO DEIZAR A MAIN PARA CHECAR ALGUMA COISA NO SERVIDOR, LOGIN DE USUARIO ETC.
+        //COMO NAO É O CASO DEIXEI VAZIA
+
         startActivity(Intent(baseContext,BuscarLivrosActivity::class.java))
+        finish()
     }
 }
